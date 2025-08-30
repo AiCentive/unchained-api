5f981d34740f3e7fd7feeaa768d8fc4554651e84
mzeeb <marc.zeeb@outlook.de>
2025-08-30 18:02:18 +0200

    FEAT: Enhance permission check to include user authentication status

--- uca_helpers.py
+++ uca_helpers.py
@@ -132,7 +132,11 @@ class UCAHelpers:
     def permission_required(permission_name, raise_exception=False):
         class PermissionRequired(permissions.BasePermission):
             def has_permission(self, request, view):
-                if not request.user or isinstance(request.user, AnonymousUser):
+                if (
+                    not request.user
+                    or isinstance(request.user, AnonymousUser)
+                    or not request.user.is_authenticated
+                ):
                     raise UCAAuthInvalid()
 
                 if not request.user.has_perm(permission_name):
